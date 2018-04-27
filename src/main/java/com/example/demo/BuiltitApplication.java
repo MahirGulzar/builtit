658ad1e7d47c5f1fd78bce224a64ac655fab7997
Mahir Gulzar <mahirgulzar@gmail.com>
2018-04-28 00:41:58 +0300

    Adding initial dependencies to builtit

---
+++ src/main/java/com/example/demo/BuiltitApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiltitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuiltitApplication.class, args);
+	}
+}
