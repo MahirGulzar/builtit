447d25fc97c6aa05aaa3ada1c563167ac920ced9
juahme <juahme@ttu.ee>
2018-05-01 03:39:06 +0300

    no message

--- src/main/java/com/example/demo/procurement/rest/controller/ProcurementRestController.java
+++ src/main/java/com/example/demo/procurement/rest/controller/ProcurementRestController.java
@@ -24,6 +24,7 @@ public class ProcurementRestController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
     public Resource<PlantHireRequestDTO> createPlantHireRequest(@RequestBody PlantHireRequestDTO phrDTO) {
+        System.out.println(phrDTO);
             return procurementService.createPlantHireRequest(phrDTO);
 
     }
