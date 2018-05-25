75663ebe352b4a141b38c019551cc89b914ceaf6
Mahir Gulzar <mahirgulzar@gmail.com>
2018-05-26 00:07:37 +0300

    Added basic auth to call rentIT

--- src/main/java/com/example/demo/procurement/integration/flows/FindPlantsFlows.java
+++ src/main/java/com/example/demo/procurement/integration/flows/FindPlantsFlows.java
@@ -1,18 +1,69 @@
 package com.example.demo.procurement.integration.flows;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.http.dsl.Http;
 import org.springframework.integration.scripting.dsl.Scripts;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+//@Configuration
+
+@Component
+@ConfigurationProperties
 @Configuration
 class FindPlantsFlows {
+
+
+    @Configuration
+    @PropertySource("classpath:credentials.properties")
+    @ConfigurationProperties
+    @Data
+    class CredentialsProperties {
+        private Map<String, Map<String, String>> credentials;
+    }
+
+    static class BasicSecureSimpleClientHttpRequestFactory extends SimpleClientHttpRequestFactory {
+        @Autowired
+        CredentialsProperties credentials;
+
+        public BasicSecureSimpleClientHttpRequestFactory() {}
+
+        public ClientHttpRequest createRequest(URI uri, HttpMethod httpMethod) throws IOException {
+            ClientHttpRequest result = super.createRequest(uri, httpMethod);
+
+            for (Map<String, String> map: credentials.getCredentials().values()) {
+                String authority = map.get("authority");
+                if (authority != null && authority.equals(uri.getAuthority())) {
+                    result.getHeaders().add("Authorization", map.get("authorization"));
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+
+    @Bean
+    public ClientHttpRequestFactory requestFactory() {
+        return new BasicSecureSimpleClientHttpRequestFactory();
+    }
+
 
     @Bean
     IntegrationFlow scatterComponent() {
@@ -64,7 +115,7 @@ class FindPlantsFlows {
                         .uriVariable("name", "payload")
                         .uriVariable("startDate", "headers.startDate")
                         .uriVariable("endDate", "headers.endDate")
-                        .httpMethod(HttpMethod.GET)
+                        .httpMethod(HttpMethod.GET).requestFactory(requestFactory())
                         .expectedResponseType(String.class)
                 )
                 .handle("findPlantsCustomTransformer", "fromHALForms")
